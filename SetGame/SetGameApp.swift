adea21f8f4896809e6050f60213224b5156f9c4b
Николай Каврецкий <kavretsky@gmail.com>
2023-02-01 22:51:28 +0400

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Nikolay Kavretsky on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
